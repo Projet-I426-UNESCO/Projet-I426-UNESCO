494643a4ac6693650bda322114b9d639c4176f1c
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-01-30 15:40:59 +0100

    Feat(AdonisJS): ajoute au migrations
    [15] [WIP]

--- unesco/database/migrations/1_create_unescos_table.ts
+++ unesco/database/migrations/1_create_unescos_table.ts
@@ -7,8 +7,32 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.string('category', 100)
+      table.string('criteria_txt', 100)
+      table.string('danger', 100)
+      table.string('date_inscribed', 100)
+
+      table.decimal('extension', 20)
+
+      table.string('http_url', 500)
+      table.string('image_url', 500)
+
+      table.string('iso_code', 10)
+      table.string('justification', 600)
+      table.string('location', 200)
+      table.string('region', 100)
+
+      table.integer('revision')
+      table.string('secondary_dates', 100)
+
+      table.string('short_description', 1200)
+      table.string('site', 200)
+
+      table.string('states', 200)
+      table.integer('transboundary')
+
+      // ex: { "lat": 48.85, "lon": 2.29 }
+      table.json('coordinates')
     })
   }
 
