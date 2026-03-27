be987c687adfb5055b7573a0e7fd7f62b2f34fb3
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-03-27 14:24:07 +0100

    Feat(Middleware): Adds auth middleware
    [30]

--- start/routes.ts
+++ start/routes.ts
@@ -11,20 +11,22 @@ import LoginController from '#controllers/auth/login_controller'
 import LogoutController from '#controllers/auth/logout_controller'
 import RegisterController from '#controllers/auth/register_controller'
 import UnescosController from '#controllers/unescos_controller'
+import AuthMiddleware from '#middleware/auth_middleware'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/', [UnescosController, 'index']).as('home')
+router.get('/', [UnescosController, 'index']).as('home').use(middleware.auth())
 
-router.get('/profile', [UnescosController, 'profile']).as('profile.show')
+router.get('/profile', [UnescosController, 'profile']).as('profile.show').use(middleware.auth())
 
 router
   .group(() => {
-    router.get('/register', [RegisterController, 'show']).as('register.show')
-    router.post('/register', [RegisterController, 'store']).as('register.store')
+    router.get('/register', [RegisterController, 'show']).as('register.show').use(middleware.guest())
+    router.post('/register', [RegisterController, 'store']).as('register.store').use(middleware.guest())
 
-    router.get('/login', [LoginController, 'show']).as('login.show')
-    router.post('/login', [LoginController, 'store']).as('login.store')
+    router.get('/login', [LoginController, 'show']).as('login.show').use(middleware.guest())
+    router.post('/login', [LoginController, 'store']).as('login.store').use(middleware.guest())
 
-    router.post('/logout', [LogoutController, 'handle']).as('logout')
+    router.post('/logout', [LogoutController, 'handle']).as('logout').use(middleware.auth())
   })
   .as('auth')
