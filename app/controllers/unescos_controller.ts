9435992ec0ff7bb062886822c98f8ea10ec3accf
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-05-01 16:01:09 +0200

    Feat(Marker): only shows the marker toggle if logged in and sends the markers (Peer programming with Gillian for 20 minutes))
    [10]

--- app/controllers/unescos_controller.ts
+++ app/controllers/unescos_controller.ts
@@ -34,8 +34,13 @@ export default class UnescosController {
     const unescos = await Unesco.query().exec()
     return view.render('pages/visits', { unescos })
   }
-  async profile({ view }: HttpContext) {
-    return view.render('pages/profile')
+  async profile({ view, auth }: HttpContext) {
+    
+    let markers = null
+    if (auth.user)
+      markers = await Marker.query().where('user_id', auth.user.id).exec()
+
+    return view.render('pages/profile', { markers })
   }
   /**
    * Display form to create a new record
